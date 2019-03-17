b2385be14e60a6c9e07e38a59ac4200d1295d771
gaganjotsingh91 <41718769+gaganjotsingh91@users.noreply.github.com>
2019-03-17 15:56:21 +1100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='datasocpandas',
+    version='1.0',
+    description='Github addition',
+    author='Gaganjot',
+    author_email='devaccgaganjotunsw@gmail.com',
+    packages=['datasocpandas'],  #same as name
+)
